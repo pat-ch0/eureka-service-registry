0e8d06d278daa9ca19836977f54147bfd45aaabb
Anthony <s35391@stagiaire.aelion.fr>
2024-02-08 15:41:25 +0100

    Add more config

--- src/main/java/org/aelion/myserviceregistry/MyServiceRegistryApplication.java
+++ src/main/java/org/aelion/myserviceregistry/MyServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package org.aelion.myserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MyServiceRegistryApplication {
 
 	public static void main(String[] args) {
