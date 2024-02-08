8d008f09befc375ed360c6676df762a17cc0fbbe
Anthony <s35391@stagiaire.aelion.fr>
2024-02-08 10:51:36 +0100

    Init

---
+++ src/test/java/org/aelion/myserviceregistry/MyServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aelion.myserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
